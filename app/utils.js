530012b73d1bf9ae9b4b0af4ec5017c81e465352
Oleksandr Menchynskyi <o.menchynskyi@geniusee.com>
2023-03-30 19:18:17 +0300

    Add pagination

--- app/utils.js
+++ app/utils.js
@@ -13,27 +13,39 @@ const databaseId = process.env.NOTION_VOCABULARY_DATABASE_ID;
 
 const numberOfWords = Number(process.env.VOCABULARY_SET_LENGTH || 15);
 
-export async function getWords() {
+export async function getWords(
+  order = "ascending",
+  startCursor = undefined,
+  words = []
+) {
+  console.log(order);
   const response = await notionClient.databases.query({
     database_id: databaseId,
     sorts: [
       {
         property: "Word",
-        direction: "ascending",
+        direction: order,
       },
     ],
+    start_cursor: startCursor,
   });
 
-  const words = response.results.map((result) => {
-    const properties = result.properties;
-    return {
-      word: properties.Word.title[0].text.content,
-      meaning:
-        properties.Translation.rich_text[0].text.content ||
-        properties.Meaning.rich_text[0].text.content ||
-        properties.Example.rich_text[0].text.content,
-    };
-  });
+  if (response.has_more)
+    return getWords(order, response.next_cursor, [
+      ...words,
+      ...response.results.map((result) => {
+        const properties = result.properties;
+        return {
+          word: properties.Word.title[0].text.content,
+          meaning:
+            properties.Translation.rich_text[0].text.content ||
+            properties.Meaning.rich_text[0].text.content ||
+            properties.Example.rich_text[0].text.content,
+        };
+      }),
+    ]);
+
+  console.log(words.length);
 
   const randomWords = [];
 
--- pages/api/words.js
+++ pages/api/words.js
@@ -1,6 +1,8 @@
 import { getWords } from "@/app/utils";
 
 export default async function handler(req, res) {
-  const words = await getWords();
+  const words = await getWords(
+    Math.random() < 0.5 ? "ascending" : "descending"
+  );
   res.status(200).json(words);
 }
