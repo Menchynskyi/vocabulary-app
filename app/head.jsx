708d228a94147cfd628ca7290873f6f091825536
Oleksandr Menchynskyi <o.menchynskyi@geniusee.com>
2023-03-31 19:24:03 +0300

    Change favicon

--- app/head.jsx
+++ app/head.jsx
@@ -3,8 +3,7 @@ export default function Head() {
     <>
       <title>Vocabulary App</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
-      <link rel="icon" sizes="16x16" href="/favicon-16x16.png" />
-      <link rel="icon" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" href="/favicon.png" />
     </>
   );
 }
