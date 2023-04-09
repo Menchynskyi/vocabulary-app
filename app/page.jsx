2d4a0a360a7d3c19dc8f87da00388199aca927fb
Oleksandr Menchynskyi <o.menchynskyi@geniusee.com>
2023-04-09 14:36:18 +0300

    refactor

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 import { WordsList } from "./WordsList";
 import { uri } from "./utils";
 
-export const revalidate = 60;
+export const revalidate = 10;
 
 async function getWords() {
   return await (await fetch(`${uri}/api/words`, { cache: "no-store" })).json();
