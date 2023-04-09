d05f4d0735e4e81d70652dece9d33c8970224f15
Oleksandr Menchynskyi <o.menchynskyi@geniusee.com>
2023-04-09 14:39:48 +0300

    test

--- app/page.jsx
+++ app/page.jsx
@@ -1,12 +1,8 @@
 import styles from "./page.module.css";
 import { WordsList } from "./WordsList";
-import { uri } from "./utils";
+import { getWords } from "./utils";
 
 export const revalidate = 10;
-
-async function getWords() {
-  return await (await fetch(`${uri}/api/words`, { cache: "no-store" })).json();
-}
 
 export default async function Home() {
   const setOfWords = await getWords();
