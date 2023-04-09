25bf7fb534c17972c1d805cbad4b036a6dad1a52
Oleksandr Menchynskyi <o.menchynskyi@geniusee.com>
2023-04-09 11:38:05 +0300

    Add revalidation path

--- app/page.jsx
+++ app/page.jsx
@@ -2,11 +2,11 @@ import styles from "./page.module.css";
 import { WordsList } from "./WordsList";
 import { uri } from "./utils";
 
+export const revalidate = 60;
+
 async function getWords() {
   return await (await fetch(`${uri}/api/words`)).json();
 }
-
-export const revalidate = 60;
 
 export default async function Home() {
   const setOfWords = await getWords();
