51b2ab4d66e1415cf76674de10c5acb65a6298bc
Oleksandr Menchynskyi <the.mngs@yahoo.com>
2024-04-21 16:54:53 +0300

    wrap searchParams component with Suspense

--- app/(cards)/layout.tsx
+++ app/(cards)/layout.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { CardsProvider } from "./_components/CardsContext";
 import { CommandMenu } from "./_components/CommandMenu";
 import { Settings } from "./_components/Settings";
+import { Suspense } from "react";
 
 type CardsLayoutProps = {
   children: React.ReactNode;
@@ -11,7 +12,9 @@ export default function CardsLayout({ children }: CardsLayoutProps) {
   return (
     <CardsProvider>
       <Header>
-        <CommandMenu />
+        <Suspense>
+          <CommandMenu />
+        </Suspense>
         <Settings />
       </Header>
       <main className="mt-16 flex justify-center sm:mt-36">{children}</main>
